e6e794836a8aabcac235f958eb9c8887e5178cad
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-06 13:23:30 +0300

    isExpanded self assign

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,18 @@
 const sections = document.querySelectorAll('section');
-let expanded = false;
 
 sections.forEach(section => {
     const div = section.querySelector('div');
     div.addEventListener('click', () => {
         const paragraph = section.querySelector('p');
         const img = section.querySelector('img');
-        if (!expanded) {
-            expanded = true;
-            paragraph.style.display = 'block';
-            img.src = '/assets/images/icon-minus.svg';
-        } else {
-            expanded = false;
+        const isExpanded = paragraph.style.display === 'block';
+
+        if (isExpanded) {
             paragraph.style.display = 'none';
             img.src = '/assets/images/icon-plus.svg';
+        } else {
+            paragraph.style.display = 'block';
+            img.src = '/assets/images/icon-minus.svg';
         }
     });
 });
